8b10825b9aa009de9d3f9818500f772faf5bffa5
Hidetake Iwata <int128@gmail.com>
2019-04-05 15:10:25 +0900

    Add test of adaptors.Cmd

--- usecases/interfaces/usecases.go
+++ usecases/interfaces/usecases.go
@@ -2,6 +2,8 @@ package usecases
 
 import "context"
 
+//go:generate mockgen -package mock_usecases -destination ../mock_usecases/mock_usecases.go github.com/int128/kubelogin/usecases/interfaces Login
+
 type Login interface {
 	Do(ctx context.Context, in LoginIn) error
 }
