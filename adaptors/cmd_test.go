4c61a71ed47020d3496c7b347c8437b3d6eeb4ef
Hidetake Iwata <int128@gmail.com>
2019-04-07 19:33:53 +0900

    Add test of adaptors.Cmd (#45)

--- adaptors/cmd_test.go
+++ adaptors/cmd_test.go
@@ -4,32 +4,81 @@ import (
 	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/int128/kubelogin/usecases/interfaces"
+	"github.com/int128/kubelogin/usecases/mock_usecases"
+	"github.com/mitchellh/go-homedir"
 )
 
-//TODO: Use gomock
-type mockLogin struct{}
-
-func (*mockLogin) Do(ctx context.Context, in usecases.LoginIn) error {
-	return nil
-}
-
 func TestCmd_Run(t *testing.T) {
-	cmd := Cmd{
-		Login: &mockLogin{},
-	}
+	const executable = "kubelogin"
+	const version = "HEAD"
 
-	t.Run("NoArg", func(t *testing.T) {
-		exitCode := cmd.Run(context.TODO(), []string{"kubelogin"}, "version")
+	t.Run("Defaults", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		ctx := context.TODO()
+
+		login := mock_usecases.NewMockLogin(ctrl)
+		login.EXPECT().
+			Do(ctx, usecases.LoginIn{
+				KubeConfig: expand(t, "~/.kube/config"),
+				ListenPort: 8000,
+			})
+
+		cmd := Cmd{
+			Login: login,
+		}
+		exitCode := cmd.Run(ctx, []string{executable}, version)
+		if exitCode != 0 {
+			t.Errorf("exitCode wants 0 but %d", exitCode)
+		}
+	})
+
+	t.Run("FullOptions", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		ctx := context.TODO()
+
+		login := mock_usecases.NewMockLogin(ctrl)
+		login.EXPECT().
+			Do(ctx, usecases.LoginIn{
+				KubeConfig:      expand(t, "~/.kube/config"),
+				ListenPort:      10080,
+				SkipTLSVerify:   true,
+				SkipOpenBrowser: true,
+			})
+
+		cmd := Cmd{
+			Login: login,
+		}
+		exitCode := cmd.Run(ctx, []string{executable,
+			"--listen-port", "10080",
+			"--insecure-skip-tls-verify",
+			"--skip-open-browser",
+		}, version)
 		if exitCode != 0 {
 			t.Errorf("exitCode wants 0 but %d", exitCode)
 		}
 	})
 
 	t.Run("TooManyArgs", func(t *testing.T) {
-		exitCode := cmd.Run(context.TODO(), []string{"kubelogin", "some"}, "version")
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+		cmd := Cmd{
+			Login: mock_usecases.NewMockLogin(ctrl),
+		}
+		exitCode := cmd.Run(context.TODO(), []string{executable, "some"}, version)
 		if exitCode != 1 {
 			t.Errorf("exitCode wants 1 but %d", exitCode)
 		}
 	})
+}
+
+func expand(t *testing.T, path string) string {
+	d, err := homedir.Expand(path)
+	if err != nil {
+		t.Fatalf("could not expand: %s", err)
+	}
+	return d
 }
