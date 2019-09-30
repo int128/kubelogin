d3082307f2a0c0cfca817ec23c22e7a1c3432f7c
Hidetake Iwata <int128@gmail.com>
2019-09-30 17:58:13 +0900

    Refactor: expand command options

--- pkg/adaptors/cmd/root.go
+++ pkg/adaptors/cmd/root.go
@@ -12,38 +12,30 @@ import (
 	"golang.org/x/xerrors"
 )
 
-// kubectlOptions represents kubectl specific options.
-type kubectlOptions struct {
+// rootOptions represents the options for the root command.
+type rootOptions struct {
 	Kubeconfig           string
 	Context              string
 	User                 string
+	ListenPort           []int
+	SkipOpenBrowser      bool
+	Username             string
+	Password             string
 	CertificateAuthority string
 	SkipTLSVerify        bool
 }
 
-func (o *kubectlOptions) register(f *pflag.FlagSet) {
+func (o *rootOptions) register(f *pflag.FlagSet) {
 	f.SortFlags = false
 	f.StringVar(&o.Kubeconfig, "kubeconfig", "", "Path to the kubeconfig file")
 	f.StringVar(&o.Context, "context", "", "The name of the kubeconfig context to use")
 	f.StringVar(&o.User, "user", "", "The name of the kubeconfig user to use. Prior to --context")
-	f.StringVar(&o.CertificateAuthority, "certificate-authority", "", "Path to a cert file for the certificate authority")
-	f.BoolVar(&o.SkipTLSVerify, "insecure-skip-tls-verify", false, "If true, the server's certificate will not be checked for validity. This will make your HTTPS connections insecure")
-}
-
-// loginOptions represents the options for Login use-case.
-type loginOptions struct {
-	ListenPort      []int
-	SkipOpenBrowser bool
-	Username        string
-	Password        string
-}
-
-func (o *loginOptions) register(f *pflag.FlagSet) {
-	f.SortFlags = false
 	f.IntSliceVar(&o.ListenPort, "listen-port", defaultListenPort, "Port to bind to the local server. If multiple ports are given, it will try the ports in order")
 	f.BoolVar(&o.SkipOpenBrowser, "skip-open-browser", false, "If true, it does not open the browser on authentication")
 	f.StringVar(&o.Username, "username", "", "If set, perform the resource owner password credentials grant")
 	f.StringVar(&o.Password, "password", "", "If set, use the password instead of asking it")
+	f.StringVar(&o.CertificateAuthority, "certificate-authority", "", "Path to a cert file for the certificate authority")
+	f.BoolVar(&o.SkipTLSVerify, "insecure-skip-tls-verify", false, "If true, the server's certificate will not be checked for validity. This will make your HTTPS connections insecure")
 }
 
 type Root struct {
@@ -52,10 +44,7 @@ type Root struct {
 }
 
 func (cmd *Root) New(ctx context.Context, executable string) *cobra.Command {
-	var o struct {
-		kubectlOptions
-		loginOptions
-	}
+	var o rootOptions
 	rootCmd := &cobra.Command{
 		Use:     executable,
 		Short:   "Login to the OpenID Connect provider and update the kubeconfig",
@@ -79,8 +68,7 @@ func (cmd *Root) New(ctx context.Context, executable string) *cobra.Command {
 			return nil
 		},
 	}
-	o.kubectlOptions.register(rootCmd.Flags())
-	o.loginOptions.register(rootCmd.Flags())
+	o.register(rootCmd.Flags())
 	cmd.Logger.AddFlags(rootCmd.PersistentFlags())
 	return rootCmd
 }
