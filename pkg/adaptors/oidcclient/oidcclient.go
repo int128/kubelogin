dca32bb9d58a3a2ccbb9b850cd0bb8225a80a36a
Hidetake Iwata <int128@gmail.com>
2019-10-29 10:14:07 +0900

    Refactor: extract parseToken method

--- pkg/adaptors/oidcclient/oidcclient.go
+++ pkg/adaptors/oidcclient/oidcclient.go
@@ -71,28 +71,7 @@ func (c *client) AuthenticateByCode(ctx context.Context, bindAddress []string, l
 	if err != nil {
 		return nil, xerrors.Errorf("could not get a token: %w", err)
 	}
-	idToken, ok := token.Extra("id_token").(string)
-	if !ok {
-		return nil, xerrors.Errorf("id_token is missing in the token response: %s", token)
-	}
-	verifier := c.provider.Verifier(&oidc.Config{ClientID: c.oauth2Config.ClientID})
-	verifiedIDToken, err := verifier.Verify(ctx, idToken)
-	if err != nil {
-		return nil, xerrors.Errorf("could not verify the id_token: %w", err)
-	}
-	if verifiedIDToken.Nonce != nonce {
-		return nil, xerrors.Errorf("nonce of ID token did not match (want %s but was %s)", nonce, verifiedIDToken.Nonce)
-	}
-	claims, err := dumpClaims(verifiedIDToken)
-	if err != nil {
-		c.logger.V(1).Infof("incomplete claims of the ID token: %w", err)
-	}
-	return &TokenSet{
-		IDToken:       idToken,
-		RefreshToken:  token.RefreshToken,
-		IDTokenExpiry: verifiedIDToken.Expiry,
-		IDTokenClaims: claims,
-	}, nil
+	return c.parseToken(ctx, token)
 }
 
 func newNonce() (string, error) {
@@ -110,25 +89,7 @@ func (c *client) AuthenticateByPassword(ctx context.Context, username, password 
 	if err != nil {
 		return nil, xerrors.Errorf("could not get a token: %w", err)
 	}
-	idToken, ok := token.Extra("id_token").(string)
-	if !ok {
-		return nil, xerrors.Errorf("id_token is missing in the token response: %s", token)
-	}
-	verifier := c.provider.Verifier(&oidc.Config{ClientID: c.oauth2Config.ClientID})
-	verifiedIDToken, err := verifier.Verify(ctx, idToken)
-	if err != nil {
-		return nil, xerrors.Errorf("could not verify the id_token: %w", err)
-	}
-	claims, err := dumpClaims(verifiedIDToken)
-	if err != nil {
-		c.logger.V(1).Infof("incomplete claims of the ID token: %w", err)
-	}
-	return &TokenSet{
-		IDToken:       idToken,
-		RefreshToken:  token.RefreshToken,
-		IDTokenExpiry: verifiedIDToken.Expiry,
-		IDTokenClaims: claims,
-	}, nil
+	return c.parseToken(ctx, token)
 }
 
 // Refresh sends a refresh token request and returns a token set.
@@ -143,6 +104,10 @@ func (c *client) Refresh(ctx context.Context, refreshToken string) (*TokenSet, e
 	if err != nil {
 		return nil, xerrors.Errorf("could not refresh the token: %w", err)
 	}
+	return c.parseToken(ctx, token)
+}
+
+func (c *client) parseToken(ctx context.Context, token *oauth2.Token) (*TokenSet, error) {
 	idToken, ok := token.Extra("id_token").(string)
 	if !ok {
 		return nil, xerrors.Errorf("id_token is missing in the token response: %s", token)
