b890007be8b38d8f3ccbcad3c4c04b0160ff2f3d
Hidetake Iwata <int128@gmail.com>
2019-10-30 20:52:33 +0900

    Refactor: make subtests

--- pkg/adaptors/cmd/cmd_test.go
+++ pkg/adaptors/cmd/cmd_test.go
@@ -17,276 +17,280 @@ func TestCmd_Run(t *testing.T) {
 	const executable = "kubelogin"
 	const version = "HEAD"
 
-	t.Run("login/Defaults", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		mockStandalone := mock_standalone.NewMockInterface(ctrl)
-		mockStandalone.EXPECT().
-			Do(ctx, standalone.Input{
-				AuthCodeOption: &authentication.AuthCodeOption{
-					BindAddress: []string{"127.0.0.1:8000", "127.0.0.1:18000"},
+	t.Run("root", func(t *testing.T) {
+		t.Run("Defaults", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			mockStandalone := mock_standalone.NewMockInterface(ctrl)
+			mockStandalone.EXPECT().
+				Do(ctx, standalone.Input{
+					AuthCodeOption: &authentication.AuthCodeOption{
+						BindAddress: []string{"127.0.0.1:8000", "127.0.0.1:18000"},
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Standalone: mockStandalone,
+					Logger:     mock_logger.New(t),
 				},
-			})
-		cmd := Cmd{
-			Root: &Root{
-				Standalone: mockStandalone,
-				Logger:     mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
-	})
-
-	t.Run("login/AuthCodeOptions", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		mockStandalone := mock_standalone.NewMockInterface(ctrl)
-		mockStandalone.EXPECT().
-			Do(ctx, standalone.Input{
-				AuthCodeOption: &authentication.AuthCodeOption{
-					BindAddress:     []string{"127.0.0.1:10080", "127.0.0.1:20080"},
-					SkipOpenBrowser: true,
-				},
-			})
-		cmd := Cmd{
-			Root: &Root{
-				Standalone: mockStandalone,
-				Logger:     mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable,
-			"--listen-port", "10080",
-			"--listen-port", "20080",
-			"--skip-open-browser",
-		}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
-	})
-
-	t.Run("login/FullOptions", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		mockStandalone := mock_standalone.NewMockInterface(ctrl)
-		mockStandalone.EXPECT().
-			Do(ctx, standalone.Input{
-				KubeconfigFilename: "/path/to/kubeconfig",
-				KubeconfigContext:  "hello.k8s.local",
-				KubeconfigUser:     "google",
-				CACertFilename:     "/path/to/cacert",
-				SkipTLSVerify:      true,
-				ROPCOption: &authentication.ROPCOption{
-					Username: "USER",
-					Password: "PASS",
-				},
-			})
-		cmd := Cmd{
-			Root: &Root{
-				Standalone: mockStandalone,
-				Logger:     mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable,
-			"--kubeconfig", "/path/to/kubeconfig",
-			"--context", "hello.k8s.local",
-			"--user", "google",
-			"--certificate-authority", "/path/to/cacert",
-			"--insecure-skip-tls-verify",
-			"-v1",
-			"--listen-port", "10080",
-			"--listen-port", "20080",
-			"--skip-open-browser",
-			"--username", "USER",
-			"--password", "PASS",
-		}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
-	})
-
-	t.Run("login/TooManyArgs", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		cmd := Cmd{
-			Root: &Root{
-				Standalone: mock_standalone.NewMockInterface(ctrl),
-				Logger:     mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(context.TODO(), []string{executable, "some"}, version)
-		if exitCode != 1 {
-			t.Errorf("exitCode wants 1 but %d", exitCode)
-		}
-	})
-
-	t.Run("get-token/Defaults", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		getToken := mock_credentialplugin.NewMockInterface(ctrl)
-		getToken.EXPECT().
-			Do(ctx, credentialplugin.Input{
-				TokenCacheDir: defaultTokenCacheDir,
-				IssuerURL:     "https://issuer.example.com",
-				ClientID:      "YOUR_CLIENT_ID",
-				AuthCodeOption: &authentication.AuthCodeOption{
-					BindAddress: []string{"127.0.0.1:8000", "127.0.0.1:18000"},
-				},
-			})
-		cmd := Cmd{
-			Root: &Root{
 				Logger: mock_logger.New(t),
-			},
-			GetToken: &GetToken{
-				GetToken: getToken,
-				Logger:   mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable,
-			"get-token",
-			"--oidc-issuer-url", "https://issuer.example.com",
-			"--oidc-client-id", "YOUR_CLIENT_ID",
-		}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
-	})
+			}
+			exitCode := cmd.Run(ctx, []string{executable}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
 
-	t.Run("get-token/AuthCodeOptions", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		getToken := mock_credentialplugin.NewMockInterface(ctrl)
-		getToken.EXPECT().
-			Do(ctx, credentialplugin.Input{
-				TokenCacheDir: defaultTokenCacheDir,
-				IssuerURL:     "https://issuer.example.com",
-				ClientID:      "YOUR_CLIENT_ID",
-				AuthCodeOption: &authentication.AuthCodeOption{
-					BindAddress:     []string{"127.0.0.1:10080", "127.0.0.1:20080"},
-					SkipOpenBrowser: true,
+		t.Run("AuthCodeOptions", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			mockStandalone := mock_standalone.NewMockInterface(ctrl)
+			mockStandalone.EXPECT().
+				Do(ctx, standalone.Input{
+					AuthCodeOption: &authentication.AuthCodeOption{
+						BindAddress:     []string{"127.0.0.1:10080", "127.0.0.1:20080"},
+						SkipOpenBrowser: true,
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Standalone: mockStandalone,
+					Logger:     mock_logger.New(t),
 				},
-			})
-		cmd := Cmd{
-			Root: &Root{
 				Logger: mock_logger.New(t),
-			},
-			GetToken: &GetToken{
-				GetToken: getToken,
-				Logger:   mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable,
-			"get-token",
-			"--oidc-issuer-url", "https://issuer.example.com",
-			"--oidc-client-id", "YOUR_CLIENT_ID",
-			"--listen-port", "10080",
-			"--listen-port", "20080",
-			"--skip-open-browser",
-		}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
-	})
+			}
+			exitCode := cmd.Run(ctx, []string{executable,
+				"--listen-port", "10080",
+				"--listen-port", "20080",
+				"--skip-open-browser",
+			}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
 
-	t.Run("get-token/FullOptions", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		getToken := mock_credentialplugin.NewMockInterface(ctrl)
-		getToken.EXPECT().
-			Do(ctx, credentialplugin.Input{
-				TokenCacheDir:  defaultTokenCacheDir,
-				IssuerURL:      "https://issuer.example.com",
-				ClientID:       "YOUR_CLIENT_ID",
-				ClientSecret:   "YOUR_CLIENT_SECRET",
-				ExtraScopes:    []string{"email", "profile"},
-				CACertFilename: "/path/to/cacert",
-				SkipTLSVerify:  true,
-				ROPCOption: &authentication.ROPCOption{
-					Username: "USER",
-					Password: "PASS",
+		t.Run("FullOptions", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			mockStandalone := mock_standalone.NewMockInterface(ctrl)
+			mockStandalone.EXPECT().
+				Do(ctx, standalone.Input{
+					KubeconfigFilename: "/path/to/kubeconfig",
+					KubeconfigContext:  "hello.k8s.local",
+					KubeconfigUser:     "google",
+					CACertFilename:     "/path/to/cacert",
+					SkipTLSVerify:      true,
+					ROPCOption: &authentication.ROPCOption{
+						Username: "USER",
+						Password: "PASS",
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Standalone: mockStandalone,
+					Logger:     mock_logger.New(t),
 				},
-			})
-		cmd := Cmd{
-			Root: &Root{
 				Logger: mock_logger.New(t),
-			},
-			GetToken: &GetToken{
-				GetToken: getToken,
-				Logger:   mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable,
-			"get-token",
-			"--oidc-issuer-url", "https://issuer.example.com",
-			"--oidc-client-id", "YOUR_CLIENT_ID",
-			"--oidc-client-secret", "YOUR_CLIENT_SECRET",
-			"--oidc-extra-scope", "email",
-			"--oidc-extra-scope", "profile",
-			"--certificate-authority", "/path/to/cacert",
-			"--insecure-skip-tls-verify",
-			"-v1",
-			"--listen-port", "10080",
-			"--listen-port", "20080",
-			"--skip-open-browser",
-			"--username", "USER",
-			"--password", "PASS",
-		}, version)
-		if exitCode != 0 {
-			t.Errorf("exitCode wants 0 but %d", exitCode)
-		}
+			}
+			exitCode := cmd.Run(ctx, []string{executable,
+				"--kubeconfig", "/path/to/kubeconfig",
+				"--context", "hello.k8s.local",
+				"--user", "google",
+				"--certificate-authority", "/path/to/cacert",
+				"--insecure-skip-tls-verify",
+				"-v1",
+				"--listen-port", "10080",
+				"--listen-port", "20080",
+				"--skip-open-browser",
+				"--username", "USER",
+				"--password", "PASS",
+			}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
+
+		t.Run("TooManyArgs", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			cmd := Cmd{
+				Root: &Root{
+					Standalone: mock_standalone.NewMockInterface(ctrl),
+					Logger:     mock_logger.New(t),
+				},
+				Logger: mock_logger.New(t),
+			}
+			exitCode := cmd.Run(context.TODO(), []string{executable, "some"}, version)
+			if exitCode != 1 {
+				t.Errorf("exitCode wants 1 but %d", exitCode)
+			}
+		})
 	})
 
-	t.Run("get-token/MissingMandatoryOptions", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		cmd := Cmd{
-			Root: &Root{
+	t.Run("get-token", func(t *testing.T) {
+		t.Run("Defaults", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			getToken := mock_credentialplugin.NewMockInterface(ctrl)
+			getToken.EXPECT().
+				Do(ctx, credentialplugin.Input{
+					TokenCacheDir: defaultTokenCacheDir,
+					IssuerURL:     "https://issuer.example.com",
+					ClientID:      "YOUR_CLIENT_ID",
+					AuthCodeOption: &authentication.AuthCodeOption{
+						BindAddress: []string{"127.0.0.1:8000", "127.0.0.1:18000"},
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Logger: mock_logger.New(t),
+				},
+				GetToken: &GetToken{
+					GetToken: getToken,
+					Logger:   mock_logger.New(t),
+				},
 				Logger: mock_logger.New(t),
-			},
-			GetToken: &GetToken{
-				GetToken: mock_credentialplugin.NewMockInterface(ctrl),
-				Logger:   mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable, "get-token"}, version)
-		if exitCode != 1 {
-			t.Errorf("exitCode wants 1 but %d", exitCode)
-		}
-	})
+			}
+			exitCode := cmd.Run(ctx, []string{executable,
+				"get-token",
+				"--oidc-issuer-url", "https://issuer.example.com",
+				"--oidc-client-id", "YOUR_CLIENT_ID",
+			}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
 
-	t.Run("get-token/TooManyArgs", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		ctx := context.TODO()
-		cmd := Cmd{
-			Root: &Root{
+		t.Run("AuthCodeOptions", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			getToken := mock_credentialplugin.NewMockInterface(ctrl)
+			getToken.EXPECT().
+				Do(ctx, credentialplugin.Input{
+					TokenCacheDir: defaultTokenCacheDir,
+					IssuerURL:     "https://issuer.example.com",
+					ClientID:      "YOUR_CLIENT_ID",
+					AuthCodeOption: &authentication.AuthCodeOption{
+						BindAddress:     []string{"127.0.0.1:10080", "127.0.0.1:20080"},
+						SkipOpenBrowser: true,
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Logger: mock_logger.New(t),
+				},
+				GetToken: &GetToken{
+					GetToken: getToken,
+					Logger:   mock_logger.New(t),
+				},
 				Logger: mock_logger.New(t),
-			},
-			GetToken: &GetToken{
-				GetToken: mock_credentialplugin.NewMockInterface(ctrl),
-				Logger:   mock_logger.New(t),
-			},
-			Logger: mock_logger.New(t),
-		}
-		exitCode := cmd.Run(ctx, []string{executable, "get-token", "foo"}, version)
-		if exitCode != 1 {
-			t.Errorf("exitCode wants 1 but %d", exitCode)
-		}
+			}
+			exitCode := cmd.Run(ctx, []string{executable,
+				"get-token",
+				"--oidc-issuer-url", "https://issuer.example.com",
+				"--oidc-client-id", "YOUR_CLIENT_ID",
+				"--listen-port", "10080",
+				"--listen-port", "20080",
+				"--skip-open-browser",
+			}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
+
+		t.Run("FullOptions", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			getToken := mock_credentialplugin.NewMockInterface(ctrl)
+			getToken.EXPECT().
+				Do(ctx, credentialplugin.Input{
+					TokenCacheDir:  defaultTokenCacheDir,
+					IssuerURL:      "https://issuer.example.com",
+					ClientID:       "YOUR_CLIENT_ID",
+					ClientSecret:   "YOUR_CLIENT_SECRET",
+					ExtraScopes:    []string{"email", "profile"},
+					CACertFilename: "/path/to/cacert",
+					SkipTLSVerify:  true,
+					ROPCOption: &authentication.ROPCOption{
+						Username: "USER",
+						Password: "PASS",
+					},
+				})
+			cmd := Cmd{
+				Root: &Root{
+					Logger: mock_logger.New(t),
+				},
+				GetToken: &GetToken{
+					GetToken: getToken,
+					Logger:   mock_logger.New(t),
+				},
+				Logger: mock_logger.New(t),
+			}
+			exitCode := cmd.Run(ctx, []string{executable,
+				"get-token",
+				"--oidc-issuer-url", "https://issuer.example.com",
+				"--oidc-client-id", "YOUR_CLIENT_ID",
+				"--oidc-client-secret", "YOUR_CLIENT_SECRET",
+				"--oidc-extra-scope", "email",
+				"--oidc-extra-scope", "profile",
+				"--certificate-authority", "/path/to/cacert",
+				"--insecure-skip-tls-verify",
+				"-v1",
+				"--listen-port", "10080",
+				"--listen-port", "20080",
+				"--skip-open-browser",
+				"--username", "USER",
+				"--password", "PASS",
+			}, version)
+			if exitCode != 0 {
+				t.Errorf("exitCode wants 0 but %d", exitCode)
+			}
+		})
+
+		t.Run("MissingMandatoryOptions", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			cmd := Cmd{
+				Root: &Root{
+					Logger: mock_logger.New(t),
+				},
+				GetToken: &GetToken{
+					GetToken: mock_credentialplugin.NewMockInterface(ctrl),
+					Logger:   mock_logger.New(t),
+				},
+				Logger: mock_logger.New(t),
+			}
+			exitCode := cmd.Run(ctx, []string{executable, "get-token"}, version)
+			if exitCode != 1 {
+				t.Errorf("exitCode wants 1 but %d", exitCode)
+			}
+		})
+
+		t.Run("TooManyArgs", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			ctx := context.TODO()
+			cmd := Cmd{
+				Root: &Root{
+					Logger: mock_logger.New(t),
+				},
+				GetToken: &GetToken{
+					GetToken: mock_credentialplugin.NewMockInterface(ctrl),
+					Logger:   mock_logger.New(t),
+				},
+				Logger: mock_logger.New(t),
+			}
+			exitCode := cmd.Run(ctx, []string{executable, "get-token", "foo"}, version)
+			if exitCode != 1 {
+				t.Errorf("exitCode wants 1 but %d", exitCode)
+			}
+		})
 	})
 }
