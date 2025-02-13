f9d1603a4d79693f46c699a4ad45d777ec864816
Hidetake Iwata <int128@gmail.com>
2025-02-13 21:24:46 +0900

    Migrate to go tool

--- tools/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-
-package main
-
-import (
-	_ "github.com/google/wire/cmd/wire"
-	_ "github.com/vektra/mockery/v2/cmd"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-)
