052a574f3c6f993b9968b06267c944ff9ac8383a
vdbe <me@ewood.dev>
2026-07-20 16:52:54 +0200

    feat(tokenexchange): add flags

--- pkg/cmd/authentication.go
+++ pkg/cmd/authentication.go
@@ -26,6 +26,14 @@ type authenticationOptions struct {
 	AuthRequestExtraParams     map[string]string
 	Username                   string
 	Password                   string
+
+	TokenExchangeResource           []string
+	TokenExchangeAudience           []string
+	TokenExchangeRequestedTokenType string
+	TokenExchangeSubjectToken       string
+	TokenExchangeSubjectTokenType   string
+	TokenExchangeActorToken         string
+	TokenExchangeActorTokenType     string
 }
 
 var allGrantType = strings.Join([]string{
@@ -35,6 +43,7 @@ var allGrantType = strings.Join([]string{
 	"password",
 	"device-code",
 	"client-credentials",
+	"token-exchange",
 }, "|")
 
 func (o *authenticationOptions) addFlags(f *pflag.FlagSet) {
@@ -46,9 +55,16 @@ func (o *authenticationOptions) addFlags(f *pflag.FlagSet) {
 	f.StringVar(&o.LocalServerCertFile, "local-server-cert", "", "[authcode] Certificate path for the local server")
 	f.StringVar(&o.LocalServerKeyFile, "local-server-key", "", "[authcode] Certificate key path for the local server")
 	f.StringVar(&o.OpenURLAfterAuthentication, "open-url-after-authentication", "", "[authcode] If set, open the URL in the browser after authentication")
-	f.StringToStringVar(&o.AuthRequestExtraParams, "oidc-auth-request-extra-params", nil, "[authcode, authcode-keyboard, client-credentials] Extra query parameters to send with an authentication request")
+	f.StringToStringVar(&o.AuthRequestExtraParams, "oidc-auth-request-extra-params", nil, "[authcode, authcode-keyboard, client-credentials, token-exchange] Extra query parameters to send with an authentication request")
 	f.StringVar(&o.Username, "username", "", "[password] Username for resource owner password credentials grant")
 	f.StringVar(&o.Password, "password", "", "[password] Password for resource owner password credentials grant")
+	f.StringSliceVar(&o.TokenExchangeResource, "token-exchange-resource", []string{}, "[token-exchange] a URI for the target resource the client intends to use")
+	f.StringSliceVar(&o.TokenExchangeAudience, "token-exchange-audience", []string{}, "[token-exchange] the audience the client intends to use")
+	f.StringVar(&o.TokenExchangeRequestedTokenType, "token-exchange-requested-token-type", "", "[token-exchange] return type desired in response, e.g. id-token or access-token")
+	f.StringVar(&o.TokenExchangeSubjectToken, "token-exchange-subject-token", "", "[token-exchange] the token to exchange (required)")
+	f.StringVar(&o.TokenExchangeSubjectTokenType, "token-exchange-subject-token-type", "", "[token-exchange] the type of token provided, e.g. id-token or access-token (required)")
+	f.StringVar(&o.TokenExchangeActorToken, "token-exchange-actor-token", "", "[token-exchange] optional token for delegated access pattern")
+	f.StringVar(&o.TokenExchangeActorTokenType, "token-exchange-actor-token-type", "", "[token-exchange] type of the actor token, e.g. id-token or access-token")
 }
 
 func (o *authenticationOptions) expandHomedir() {
@@ -89,6 +105,9 @@ func (o *authenticationOptions) grantOptionSet() (s authentication.GrantOptionSe
 			endpointparams[k] = []string{v}
 		}
 		s.ClientCredentialsOption = &client.GetTokenByClientCredentialsInput{EndpointParams: endpointparams}
+	case o.GrantType == "token-exchange":
+		// TODO(vdbe): implement this
+		err = fmt.Errorf("grant-type %s is not implemented", allGrantType)
 	default:
 		err = fmt.Errorf("grant-type must be one of (%s)", allGrantType)
 	}
